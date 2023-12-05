715bc421191cdadfae637461c6bf214246ca98eb
Artem <hartem472@gmail.com>
2023-12-05 01:16:15 +0100

    solution

--- list/urls.py
+++ list/urls.py
@@ -17,10 +17,10 @@ urlpatterns = [
     path("task/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
     path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
 
-    path("tags", TagListView.as_view(), name="tag-list"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
     path("tag/create/", TagCreateView.as_view(), name="tag-create"),
     path("tag/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
     path("tag/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
 ]
 
-app_name = "taxi"
+app_name = "list"
